6fa44e9c193101dbab3ee0a014a010013e0f78c0
GEORGE-EDIR <georgetaban256@gmail.com>
2026-09-16 11:52:24 +0300

    Add files via upload

---
+++ Week_2_sales_Assignment.sql.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE sales;
+
+USE sales;
+SELECT DATABASE();
+SHOW TABLES;
+
+SELECT checkNumber, paymentDate, amount
+FROM payments;
+
+SELECT orderDate, requiredDate, status
+FROM orders
+WHERE status = 'In Process'
+ORDER BY orderDate DESC;
+
+SELECT firstName, lastName, email
+FROM employees
+WHERE jobTitle = 'Sales Rep'
+ORDER BY employeeNumber DESC;
+
+SELECT *
+FROM offices;
+
+SELECT productName, quantityInStock
+FROM products
+ORDER BY buyPrice ASC
+LIMIT 5;
